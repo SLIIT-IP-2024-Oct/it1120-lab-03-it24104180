cc31798726ea15e7a14a875dce7dc1bf856fcfa5
it24104180 <it24104180@my.sliit.lk>
2024-11-21 13:09:01 +0530

    Add IT24104180Lab3Q1A.java to Q1A

---
+++ Q1A/IT24104180Lab3Q1A.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class IT24104180Lab3Q1A  {
+    public static void main(String[] args) {
+               Scanner scanner=new Scanner (System.in);
+        System.out.println("Enter the number of kilograms");
+        int num_kilo=scanner.nextInt();
+
+        System.out.println("Enter 1KG price");
+        int kg_price=scanner.nextInt();
+
+        int total_price=(kg_price)*(num_kilo);
+        System.out.println("Total Price="+total_price);
+    }
+}
